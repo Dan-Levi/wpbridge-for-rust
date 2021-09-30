773456e895e543f84c6a472ead945ed498e8094c
Dan-Levi Tømta <danbannan@gmail.com>
2021-09-30 20:45:49 +0200

    Update constants.php
    added constant array holding the column names for the server variables.

--- inc/constants.php
+++ inc/constants.php
@@ -4,6 +4,17 @@ define( 'WPBRIDGE_PLUGIN_VERSION', $this->plugin_version);
 
 define( 'WPBRIDGE_SETTINGS_TABLE', $wpdb->prefix . 'wpbridge_settings' );
 define( 'WPBRIDGE_PLAYER_STATS_TABLE', $wpdb->prefix . 'wpbridge_player_stats' );
+define( 'WPBRIDGE_SERVER_STATS', [
+    'port',
+    'level',
+    'identity',
+    'seed',
+    'worldsize',
+    'maxplayers',
+    'hostname',
+    'description',
+    'updated'
+]);
 define( 'WPBRIDGE_PLAYER_STATS', [
     'joins',            
     'leaves',               
